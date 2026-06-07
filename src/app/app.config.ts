c3f5339acb095c2f757b62d60b56fa40e90b93f3
siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2026-06-08 00:24:49 +0200

    installed store devtools, set up basic app state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideStore(), // registers reducers with the ngrx store
     provideEffects() // registers effects with the ngrx store
-  ]
+    ,
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }) // enables store devtools
+]
 };
---
+++ src/app/core/state/app.state.ts
@@ -0,0 +1,6 @@
+export interface AppState {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+}
